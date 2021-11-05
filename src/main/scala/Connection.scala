fc5b06cdeb32732c2f476ca9e4d757f2a5e0c021
Louis Duller <louis.mitchell.duller@gmail.com>
2021-11-04 20:57:58 -0400

    Added Connection object and function stubs

---
+++ src/main/scala/Connection.scala
@@ -0,0 +1,14 @@
+// Connection object represents SparkSession to Hive through Apache Spark
+object Connection {
+  def connect() : Unit = {
+
+  }
+
+  def disconnect(): Unit = {
+
+  }
+
+  def getConnection(): Unit = {
+
+  }
+}
