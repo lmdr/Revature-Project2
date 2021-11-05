634d3abd4867bac0418ec201dbea1210130d7a07
Louis Duller <louis.mitchell.duller@gmail.com>
2021-11-04 21:02:01 -0400

    Added Menu object and function stubs

---
+++ src/main/scala/Menu.scala
@@ -0,0 +1,14 @@
+// Menu represents user menus
+object Menu {
+  def display_main_menu(): Unit = {
+    println("display_main_menu()")
+  }
+
+  def display_admin_menu(): Unit = {
+    println("display_admin_menu()")
+  }
+
+  def display_user_menu(): Unit = {
+    println("display_user_menu()")
+  }
+}
