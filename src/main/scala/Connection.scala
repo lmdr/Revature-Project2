13b59771e1e22dbddce0ee65ec4660c8080df36c
Louis Duller <louis.mitchell.duller@gmail.com>
2021-11-07 18:14:25 -0500

    Added SparkSession to Connection

--- src/main/scala/Connection.scala
+++ src/main/scala/Connection.scala
@@ -1,15 +1,28 @@
+import org.apache.spark.sql.SparkSession
+
 // Connection object represents SparkSession to Hive through Apache Spark
 object Connection {
+  private var _spark: org.apache.spark.sql.SparkSession = _
+
   def connect() : Unit = {
     println("connect()")
-    //System.setProperty("hadoop.home.dir", "C:\\hadoop")
+    System.setProperty("hadoop.home.dir", "C:\\hadoop")
+    _spark = SparkSession
+      .builder()
+      .appName("Project 1")
+      .config("spark.master", "local")
+      .enableHiveSupport()
+      .getOrCreate()
+    //_spark.sparkContext.setLogLevel("ERROR")
   }
 
   def disconnect(): Unit = {
     println("disconnect()")
+    _spark.stop()
   }
 
-  def getConnection(): Unit = {
+  def get_connection(): org.apache.spark.sql.SparkSession = {
     println("getConnection()")
+    _spark
   }
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,8 @@
 object Main {
   def main(args: Array[String]): Unit = {
+    Connection.connect()
     Menu.display_welcome_message()
     Menu.display_main_menu()
+    Connection.disconnect()
   }
 }
